7607d4cab608816565a5526944eeed6a8eab6768
Edubew <edubew98@gmail.com>
2022-05-10 22:20:43 +0300

    Check for linters errors

--- script.js
+++ script.js
@@ -1,30 +1,27 @@
 // Gain access  to the harmburger button and the mobile menu list
 
-const hamburger = document.querySelector(".hamburger");
-const navbarLink = document.querySelector(".navbar-link");
-const logo = document.querySelector(".logo");
-var click =1;
+const hamburger = document.querySelector('.hamburger');
+const navbarLink = document.querySelector('.navbar-link');
+const logo = document.querySelector('.logo');
+let click = 1;
 
 // Add an event listener to the hamburger menu
 hamburger.addEventListener('click', () => {
-    hamburger.classList.toggle('active');
-     navbarLink.classList.toggle('active');
-     if(click==1){
-         logo.innerText="";
-         click--;
-     }
-     else {
-         logo.innerText= "Edube";
-         click++;
-     }
+  hamburger.classList.toggle('active');
+  navbarLink.classList.toggle('active');
+  if (click === 1) {
+    logo.innerText = '';
+    click -= 1;
+  } else {
+    logo.innerText = 'Edube';
+    click += 1;
+  }
 });
 
-// Set the 
+// Set the
 document.querySelectorAll('.nav-links').forEach((e) => e.addEventListener('click', () => {
-    hamburger.classList.remove('active');
-    navbarLink.classList.remove('active');
-     logo.innerText = 'Edube';
-     click++;
-  }));
-
-
+  hamburger.classList.remove('active');
+  navbarLink.classList.remove('active');
+  logo.innerText = 'Edube';
+  click += 1;
+}));
