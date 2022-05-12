755aac0534cea087cda10f983d72aa08eb2fcbdf
Edubew <edubew98@gmail.com>
2022-05-12 15:36:19 +0300

    Add the window popup in html and css and rendered it dynamically with javascript

--- popup.js
+++ popup.js
@@ -5,33 +5,49 @@ const projects =[
         id: 1,
         featuredImage: 'img-placeholder.png',
         title: 'Multi-Post Stories',
+        mobileImage: 'mobile-modal.png',
+        desktopImage: 'desktop-modal.png',
+        desc: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting  ever since the 1500s, when an unknown printer took a galley of type veris lapoa todoe.',
         description:
         ' A daily selection of privately personalized reads; no accounts or sign-ups required has been the industrys standard dummy textever since the 1500s when an unknown printer took a standard dummy text.',
         technologies: ['css', 'html', 'Bootstrap', 'Ruby'],
+        technologies2: ['html', 'Ruby on rails', 'css', 'Github'],
     },
     {
         id: 2,
         featuredImage: 'img-placeholder.png',
         title: 'Multi-Post Stories',
+        mobileImage: 'mobile-modal.png',
+        desktopImage: 'desktop-modal.png',
+        desc: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting  ever since the 1500s, when an unknown printer took a galley of type veris lapoa todoe.',
         description:
         ' A daily selection of privately personalized reads; no accounts or sign-ups required has been the industrys standard dummy textever since the 1500s when an unknown printer took a standard dummy text.',
         technologies: ['css', 'html', 'Bootstrap', 'Ruby'],
+        technologies2: ['html', 'Ruby on rails', 'css', 'Github'],
     },
     {
         id: 3,
         featuredImage: 'img-placeholder.png',
         title: 'Multi-Post Stories',
+        mobileImage: 'mobile-modal.png',
+        desktopImage: 'desktop-modal.png',
+        desc: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting  ever since the 1500s, when an unknown printer took a galley of type veris lapoa todoe.',
         description:
         ' A daily selection of privately personalized reads; no accounts or sign-ups required has been the industrys standard dummy textever since the 1500s when an unknown printer took a standard dummy text.',
         technologies: ['css', 'html', 'Bootstrap', 'Ruby'],
+        technologies2: ['html', 'Ruby on rails', 'css', 'Github'],
     },
     {
         id: 4,
         featuredImage: 'img-placeholder.png',
         title: 'Multi-Post Stories',
+        mobileImage: 'mobile-modal.png',
+        desktopImage: 'desktop-modal.png',
+        desc: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting  ever since the 1500s, when an unknown printer took a galley of type veris lapoa todoe.',
         description:
         ' A daily selection of privately personalized reads; no accounts or sign-ups required has been the industrys standard dummy textever since the 1500s when an unknown printer took a standard dummy text.',
         technologies: ['css', 'html', 'Bootstrap', 'Ruby'],
+        technologies2: ['html', 'Ruby on rails', 'css', 'Github'],
     },
 ];
 
@@ -45,7 +61,7 @@ const mainContainer = projects.map((work)=> ` <div class="projects-container">
     <p>${work.description}</p>
     <ul class="list-tags">${work.technologies.map((technology)=> `<li class="border-right">${technology}</li>`).join('')}
     </ul>
-    <button type="button">See Project</button>
+    <button type="button" onclick="showModal(${work.id})">See Project</button>
   </div>
 </div>
 </div>`).join('');
@@ -63,19 +79,33 @@ const windowContent = (modal)=> {
   const template= document.createElement('template');
   template.innerHTML = ` <div class="heading-container">
   <h2>${modal.title}</h2>
-  <button class="close"><i class="fa-solid fa-xmark"></i></button>
- </div>
- <div class="modal-content">
-   <img src="images/${modal.mobileImage}" class="image-placeholder mobile" alt="">
-   <img src="images/${modal.desktopImage}" class="image-placeholder desktop" alt="">
-   <p>${modal.desc}</p>
-   <ul class="list-tags">${modal.technologies.map((technology)=>`<li class="border-right">${technology}</li>`).join('')}
-   </ul>
-   <div class="submit">
-     <button type="button">See Live<img src="images/Icon.png" alt=""></button>
-     <button type="button">See Source<i class="fa-brands fa-github"></i></button>
-   </div>
- </div>`;
+  <button class="close" onclick="closeModal()" type="button"><i class="fa-solid fa-xmark"></i></button>
+</div>
+<div class="modal-content">
+  <img src="images/${modal.mobileImage}" class="image-placeholder mobile" alt="">
+  <img src="images/${modal.desktopImage}" class="image-placeholder desktop" alt="">
+  <p>${modal.desc}</p>
+  <ul class="list-tags">${modal.technologies2.map((technology2)=> `<li class="border-right">${technology2}</li>`).join('')}
+  </ul>
+  <div class="submit">
+    <button>See Live<img src="images/Icon.png" alt=""></button>
+    <button>See Source<i class="fa-brands fa-github"></i></button>
+  </div>
+</div>`
+modalWindow.appendChild(template.content);
+wrapContainer.classList.add('active');
+};
 
- 
+const showModal= (id)=> {
+  projects.find((item)=>{
+    if(Number(id)=== Number(item.id)){
+      windowContent(item);
+    }
+    return undefined;
+  });
+};
+
+function closeModal() {
+  modalWindow.innerHTML = '';
+  wrapContainer.classList.remove('active');
 }
